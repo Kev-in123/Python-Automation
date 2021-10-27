f120fdf8ecba859db7a423a7843aa545831faa0b
Kevin Cai <75402062+Kev-in123@users.noreply.github.com>
2021-10-27 17:19:46 -0400

    Update Automation on Interactive Websites - Challenge.py

--- src/Automation on Interactive Websites - Challenge.py
+++ src/Automation on Interactive Websites - Challenge.py
@@ -1,1 +1,21 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
 
+driver = webdriver.Firefox()
+driver.get("https://arithmetic.zetamac.com/game?key=72740d67")
+time.sleep(3)
+
+while 1:
+  question = driver.find_element(By.CSS_SELECTOR, "html body div#game div.banner div.start span.problem") 
+  answer = driver.find_element(By.CSS_SELECTOR, ".answer")
+  question = question.text
+  question = question.replace("÷", "/")
+  question = question.replace("×", "*")
+  question = question.replace("–", "-")
+  answer.send_keys(int(eval(question)))
+  answer.send_keys(Keys.RETURN)
+
+time.sleep(3)
+driver.close()
